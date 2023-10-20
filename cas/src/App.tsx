139945a0150b41dab8c943ac5d5783517c8a2e4c
mperdikeas <mperdikeas@gmail.com>
2023-10-20 14:27:03 +0300

    navigation with classical links

--- cas/src/App.tsx
+++ cas/src/App.tsx
@@ -8,6 +8,16 @@ function App() {
   return (
     <div>
       <h1>CAS &hellip; Central Authentication Server</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href={`/login`}>Login</a>
+          </li>
+          <li>
+            <a href={`/change-password`}>Change password</a>
+          </li>
+        </ul>
+      </nav>
       <Outlet/>
     </div>
   );
