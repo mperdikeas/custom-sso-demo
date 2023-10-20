9518c2c06920b14031a86ac3ce949ed789cca49b
mperdikeas <mperdikeas@gmail.com>
2023-10-20 14:52:54 +0300

    first version when a cookie is set

--- cas/src/login.tsx
+++ cas/src/login.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+
+import { Button, Form, Input }  from 'antd';
 
 
 
 function Login() {
+
+  const onFinish = useCallback((x: unknown)=>{
+    // @ts-expect-error
+    const {username, password}: {username: string, password: string} = x;
+    if ((username==='admin') && (password==='admin')) {
+      const cookieName = 'cas-token';
+      const cookieValue = '666';
+      const myDate = new Date();
+      myDate.setMonth(myDate.getMonth() + 12);
+      const cookie = cookieName +"=" + cookieValue + ";expires=" + myDate + ";domain=.cognitera.gr;path=/";
+      document.cookie = cookie;
+    } else {
+
+    }
+
+  }, []);
   return (
-    <div>
-      display a login form
-    </div>
+    <Form
+    name='basic'
+    onFinish={onFinish}
+    onFinishFailed={()=>{}}
+    autoComplete='off'
+    >
+
+    <Form.Item
+      label='Username'
+      name='username'
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label='Password'
+      name='password'
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+    </Form>
   );
 }
 
