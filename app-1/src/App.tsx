18bb58021d30d9e262047532d7e00b74a229fc7f
mperdikeas <mperdikeas@gmail.com>
2023-10-20 16:08:32 +0300

    app-2 successful reads the token if present

--- app-1/src/App.tsx
+++ app-1/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import Cookies from 'js-cookie';
 
 function App() {
+  const [is_token_present, set_is_token_present] = useState(false);
+  useEffect(()=>{
+    const token: string | undefined = Cookies.get('cas-token');
+    if (token !== undefined) {
+      set_is_token_present(true);
+    }
+  }, []);
   return (
     <div>
-      Application 1
+      <b>Application 1</b><br/>
+      Token is present? {is_token_present?<b>yes</b>: <b>no</b>}
     </div>
   );
 }
